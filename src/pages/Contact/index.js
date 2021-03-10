fbe4223b049d6c0c72b131ba38ff76526e449edd
Melvin Kosisochukwu <melvinnnamani@gmail.com>
2021-03-10 16:18:34 +0100

    All pages component built

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -64,7 +64,7 @@ export default function Contact() {
         </a>
       </Section>
 
-      <ContactComponent title="Drop a Message" header="Start a Conversation " />
+      <ContactComponent title="Drop a Message" header="Start a Conversation" />
     </>
   );
 }
